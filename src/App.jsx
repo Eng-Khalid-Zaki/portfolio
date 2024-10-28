273e76d987f95249e18067777e8424b70db8b0c8
Khalid Zaki <khalid@Khalid.khalid>
2024-10-28 14:58:59 +0300

    Modify: All CSS for the entire project & Add: to-top button

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import { useState, useEffect } from "react";
+import { Link as ScrollLink, Element } from "react-scroll";
 import Header from "./components/header/Header";
 import Hero from "./components/hero/Hero";
 import Projects from "./components/projects/Projects";
 import Contact from "./components/contact-us/Contact";
 import Footer from "./components/footer/Footer";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCaretUp } from "@fortawesome/free-solid-svg-icons";
+
 function App() {
   const [theme, setTheme] = useState("dark");
   const [width, setWidth] = useState(window.innerWidth);
@@ -14,21 +18,26 @@ function App() {
     window.addEventListener("resize", handleResize);
     return () => window.removeEventListener("resize", handleResize);
   }, []);
+
   const toggleTheme = () => {
-    if (theme === "dark") {
-      setTheme("light");
-    } else {
-      setTheme("dark");
-    }
+    setTheme((prevTheme) => (prevTheme === "dark" ? "light" : "dark"));
   };
 
   return (
     <div className="container">
-      <Header theme={theme} toggleTheme={toggleTheme} screenWidth={width} />
+      <Element name="top">
+        <Header theme={theme} toggleTheme={toggleTheme} screenWidth={width} />
+      </Element>
       <Hero theme={theme} screenWidth={width} />
+
       <Projects theme={theme} screenWidth={width} />
       <Contact theme={theme} screenWidth={width} />
       <Footer theme={theme} screenWidth={width} />
+      <ScrollLink to="top" smooth={true} duration={500}>
+        <button className="to-top">
+          <FontAwesomeIcon icon={faCaretUp} />
+        </button>
+      </ScrollLink>
     </div>
   );
 }
